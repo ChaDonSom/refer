d3f2a4e29297e96f8a6a740f99256f8972134dc2
ChaDonSom <chadonsom@gmail.com>
2020-07-03 23:11:06 -0400

    Add a few CKEditor plugins

--- resources/js/components/Editor.vue
+++ resources/js/components/Editor.vue
@@ -5,12 +5,19 @@
 <script>
 import { ref, onMounted } from '@js/vue'
 import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor'
-// import Mention from '@ckeditor/ckeditor5-mention/src/mention'
+import Mention from '@ckeditor/ckeditor5-mention/src/mention'
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
-// import ContextualBalloon from '@ckeditor/ckeditor5-ui/src/panel/balloon/contextualballoon'
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
+import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
+import Code from '@ckeditor/ckeditor5-basic-styles/src/code';
+import Heading from '@ckeditor/ckeditor5-heading/src/heading';
+import Link from '@ckeditor/ckeditor5-link/src/link';
+import Indent from '@ckeditor/ckeditor5-indent/src/indent';
+import IndentBlock from '@ckeditor/ckeditor5-indent/src/indentblock';
+import RemoveFormat from '@ckeditor/ckeditor5-remove-format/src/removeformat';
 
 export default {
   data() { return {
@@ -20,14 +27,24 @@ export default {
       plugins: [
         Essentials,
         Paragraph,
+        RemoveFormat,
+        Heading,
         Bold,
         Italic,
-        // ContextualBalloon,
-        // Mention,
+        Underline,
+        Strikethrough,
+        Code,
+        Mention,
+        Link,
+        Indent,
+        IndentBlock,
       ],
       toolbar: [
-        'bold', 'italic'
+        'heading', 'bold', 'italic', 'underline', 'strikethrough', 'link', '|', 'removeFormat', '|', 'undo', 'redo'
       ],
+      mention: {
+
+      }
     }
   }},
 }
